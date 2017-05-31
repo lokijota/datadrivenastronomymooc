066c70f057a3f5dec834bf6328a3629088570570
lokijota <joao.pedro.martins@gmail.com>
2017-05-31 00:25:47 +0000

    Comparing median and mean (used numpy)

---
+++ wk1/ComparingMedianAndMean.py
@@ -0,0 +1,18 @@
+# Write your list_stats function here.
+import numpy as np
+
+def list_stats(values):
+  return (np.median(values), np.mean(values))
+
+
+# You can use this to test your function.
+# Any code inside this `if` statement will be ignored by the automarker.
+if __name__ == '__main__':
+  # Run your function with the first example in the question.
+  m = list_stats([1.3, 2.4, 20.6, 0.95, 3.1, 2.7])
+  print(m)
+
+  # Run your function with the second example in the question
+  m = list_stats([1.5])
+  print(m)
+
