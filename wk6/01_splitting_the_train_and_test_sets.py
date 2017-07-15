a20b98bcf7784d15a2fa5d8cbfccb143adb5591e
lokijota <joao.pedro.martins@gmail.com>
2017-07-15 00:14:06 +0000

    Added some comments to 1st exercise

--- wk6/01_splitting_the_train_and_test_sets.py
+++ wk6/01_splitting_the_train_and_test_sets.py
@@ -8,10 +8,10 @@ def splitdata_train_test(data, fraction_training):
   np.random.shuffle(data)
 
   #find split point
-  training_rows = math.floor(len(data) * fraction_training)
+  training_rows = math.floor(len(data) * fraction_training) #int(...) would be enough
   
-  training_set = data[0:training_rows]
-  testing_set = data[training_rows:len(data)]
+  training_set = data[0:training_rows] #or data[:training_rows]
+  testing_set = data[training_rows:len(data)] #or data[training_rows:]
   return (training_set, testing_set)
 
 if __name__ == "__main__":
